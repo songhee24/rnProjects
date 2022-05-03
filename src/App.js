fbbed6e16ebeea21b28bfc9b9dd840087dcb6f28
songhee24 <akaratel61@gmail.com>
2022-05-03 15:09:57 +0600

    base layout screens created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import type { Node } from 'react'
 import { SafeAreaView, StyleSheet } from 'react-native'
+import { UserSignInScreen } from './screens/auth/UserSignIn.screen'
 
 const App: () => Node = () => {
-  return <SafeAreaView />
+  return (
+    <SafeAreaView>
+      <UserSignInScreen />
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({})
---
+++ src/screens/auth/UserSignIn.screen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const UserSignInScreen = () => {
+  return (
+    <View>
+      <Text>Please Register !</Text>
+    </View>
+  )
+}
