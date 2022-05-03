c0cbfed13853eefd2066847c590ad72bb73cc80f
songhee24 <akaratel61@gmail.com>
2022-05-03 16:12:16 +0600

    text component logic with prop types validity

--- src/components/UI/AppText.js
+++ src/components/UI/AppText.js
@@ -1,7 +1,20 @@
+import React from 'react'
 import { Text } from 'react-native'
+import { PropTypes } from 'prop-types'
+import { THEME_FONTS } from '../../utils/theme'
 
-export const AppText = () => {
-  return <Text style={styles.root} />
+export const AppText = ({ fontFamily, size, color }) => {
+  const fontFamilyStyle = React.useMemo(() => {
+    if (fontFamily === 'medium') {
+      return { fontFamily: THEME_FONTS.fontFamilyMedium }
+    }
+
+    return { fontFamily: THEME_FONTS.fontFamilyRegular }
+  }, [fontFamily])
+
+  const fontSizeStyle = React.useMemo(() => ({ fontSize: size }), [size])
+
+  return <Text style={[styles.root, fontFamilyStyle, fontSizeStyle]} />
 }
 
 const styles = StyleSheet.create({
@@ -11,3 +24,9 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
   },
 })
+
+AppText.propTypes = {
+  fontFamily: PropTypes.oneOf(['medium, light']).isRequired,
+  size: PropTypes.oneOf([10, 12, 14, 16, 18, 20]).isRequired,
+  color: PropTypes.string.isRequired,
+}
